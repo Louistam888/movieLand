8b865441001357710e8014d68949b864e03db105
louistam888 <louiscodes888@gmail.com>
2022-12-09 09:31:27 -0500

    fixed unused variable

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import MovieCard from "./MovieCard"
 
 const API_URL = "http://www.omdbapi.com?apikey=aa0ec5ea"
 
-const movie1 = {
-  "Title": "Italian Spiderman",
-  "Year": "2007",
-  "imdbID": "tt2705436",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
-}
-
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [movies, setMovies] = useState([]);
